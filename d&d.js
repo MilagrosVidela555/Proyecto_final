c175af360f25701337a5cf99053558ccd2c320d1
MilagrosVidela55 <milagrosvidela2023@gmail.com>
2023-11-21 00:38:11 -0300

    Se agrega el Proyecto

--- d&d.js
+++ d&d.js
@@ -1,17 +1,14 @@
-let img1 = document.querySelector("#img1");
-let img2 = document.querySelector("#img2");
-let img3 = document.querySelector("#img3");
-
+let imagenes = document.querySelectorAll('.card_img img');
 let reinicio = document.querySelector(".btn_reiniciar")
 let destinos = document.querySelectorAll(".card_soltar");
 
-img1.addEventListener('dragstart', iniciaTraslado);
-img2.addEventListener('dragstart', iniciaTraslado);
-img3.addEventListener('dragstart', iniciaTraslado);
+imagenes.forEach(imagen => {
+    imagen.addEventListener('dragstart', iniciaTraslado);
+});
 
-img1.addEventListener('dragend', finDelTraslado);
-img2.addEventListener('dragend', finDelTraslado);
-img3.addEventListener('dragend', finDelTraslado);
+imagenes.forEach(imagen => {
+    imagen.addEventListener('dragend', finDelTraslado);
+});
 
 
 reinicio.addEventListener('click', reinicioBoton)
@@ -21,12 +18,10 @@ function iniciaTraslado(evento) {
 };
 
 function finDelTraslado(evento) {
-    let img1 = evento.target;
-    let img2 = evento.target;
-    let img3 = evento.target;
-    img1.style.visibility= "hidden"
-    img2.style.visibility= "hidden"
-    img3.style.visibility= "hidden"
+    let imagenes = evento.target;
+    imagenes.style.display= 'none';
+    let contenedorDestino = evento.target.closest('.card_img');
+    contenedorDestino.remove();
 };
 
 /* en los destinos */
@@ -39,6 +34,7 @@ function soltarElemento(evento) {
     prevenirDefault(evento);
     let dataimg = evento.dataTransfer.getData("text/uri-list");
     evento.target.innerHTML = `<img src="${dataimg}" />`;
+
 };
 
 function prevenirDefault(evento) {
